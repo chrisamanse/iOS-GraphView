3ed2a1c6a437abb73d1a23947516a40d5cd1c541
Trainner02 <trainner02@DT-MM-Yiel.local>
2014-05-27 09:05:51 +0800

    Updated comments for properties

--- GraphView/ACScatterPlotView.h
+++ GraphView/ACScatterPlotView.h
@@ -21,9 +21,9 @@
 
 @property (strong, nonatomic, readonly) UIImageView *imageViewScatterPlot;
 @property (strong, nonatomic) NSNumber *resolution; // Sets image resolution scale from container view size. If not set, default is set to screen's scale ([UIScreen mainScreen].scale), results to same size of container in pixels
-@property (strong, nonatomic) NSNumber *padding;
-@property (strong, nonatomic) NSNumber *leftInset;
-@property (strong, nonatomic) NSNumber *bottomInset;
+@property (strong, nonatomic) NSNumber *padding; // Top, right, bottom, left padding of graph (including axes and labels)
+@property (strong, nonatomic) NSNumber *leftInset; // Left inset - serves as a space for y axis labels
+@property (strong, nonatomic) NSNumber *bottomInset; // Bottom inset - space for x axis labels
 
 // Line Widths
 @property (strong, nonatomic) NSNumber *lineWidth;
@@ -37,13 +37,15 @@
 @property (strong, nonatomic) UIColor *overlayIndicatorColor;
 @property (strong, nonatomic) UIColor *axesLineColor;
 
-@property (strong, nonatomic) NSNumber *stepSize;
+@property (strong, nonatomic) NSNumber *stepSize; // Determines x values to be passed to dataSource
 
-@property (strong, nonatomic) NSNumber *rotationAngleOfLabels;
+@property (strong, nonatomic) NSNumber *rotationAngleOfLabels; // Tilt x axis labels. If not set, or set to 0, labels will not be tilted
 
+// Properties for axes (ticks, label font size)
 @property (strong, nonatomic) ACAxis *xAxis;
 @property (strong, nonatomic) ACAxis *yAxis;
 
+// Range of axes - if y axis range is not set, its min and max are generated automatically based on dataSource
 @property (strong, nonatomic) ACAxisRange *xAxisRange;
 @property (strong, nonatomic) ACAxisRange *yAxisRange;
 
